dd9c3fc7b614564a532c705c8d0a58164f5830ac
Anton0694 <91780319+Anton0694@users.noreply.github.com>
2023-03-09 11:40:34 +0200

    fix loader)

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,18 +1,32 @@
- import { ThreeDots } from  'react-loader-spinner'
+import { ThreeDots } from 'react-loader-spinner';
 
 export const Loader = () => {
-    return (
-       
-        <ThreeDots 
-gap='50%'
-height="80" 
-width="80" 
-radius="9"
-color="#4fa94d" 
-ariaLabel="three-dots-loading"
-wrapperStyle={{}}
-wrapperClassName=""
-visible={true}
- />
-    )
-}
+  return (
+    <div
+      style={{
+        position: 'fixed',
+        top: '90%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)'
+      }}
+    >
+      <ThreeDots
+        height="80"
+        width="80"
+        radius="9"
+        color="#4fa94d"
+        ariaLabel="three-dots-loading"
+        wrapperStyle={{}}
+        display="flex"
+        wrapperClassName="Loader"
+        visible={true}
+      />
+    </div>
+  );
+};
+
+
+
+
+
+
