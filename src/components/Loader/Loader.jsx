387b3d246f6de84c7b323b780d50960678eca468
Anton0694 <91780319+Anton0694@users.noreply.github.com>
2023-03-09 12:18:23 +0200

    loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -2,26 +2,24 @@ import { ThreeDots } from 'react-loader-spinner';
 
 export const Loader = () => {
   return (
-    <div
-      style={{
-        position: 'fixed',
-        top: '90%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)'
-      }}
-    >
+    
       <ThreeDots
         height="80"
         width="80"
         radius="9"
         color="#4fa94d"
         ariaLabel="three-dots-loading"
-        wrapperStyle={{}}
+      wrapperStyle={{
+        position: 'fixed',
+        top: '90%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)'
+      }}
         display="flex"
-        wrapperClassName="Loader"
+        wrapperClassName=""
         visible={true}
       />
-    </div>
+    
   );
 };
 
